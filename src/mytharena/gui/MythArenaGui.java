59b316b6685ad6033ffa2b565a12e13ee6b69a6a
legendnightt <alex@legend.sh>
2022-04-15 20:14:31 +0200

    feat: gui options interaction

--- src/mytharena/gui/MythArenaGui.java
+++ src/mytharena/gui/MythArenaGui.java
@@ -1,6 +1,8 @@
 package mytharena.gui;
 
 import javax.swing.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * MythArenaGui final class
@@ -11,6 +13,10 @@ public final class MythArenaGui {
      * MythArenaGuiScreen mythArenaGuiScreen
      */
     private MythArenaGuiScreen mythArenaGuiScreen;
+    /**
+     * Queue Character buffer
+     */
+    private Queue<Character> buffer = new LinkedList();
 
     /**
      * MythArenaGui class constructor
@@ -42,6 +48,45 @@ public final class MythArenaGui {
      */
     public void setImage(ImageIcon image) {
         this.mythArenaGuiScreen.setImage(image);
+    }
+
+    /**
+     * Sets specified option message
+     * @param number int number
+     * @param message String message
+     */
+    public void setOption(int number, String message) {
+        if (number <= 9 && number >= 0) {
+            this.mythArenaGuiScreen.setOption(number, message);
+        } else {
+            throw new RuntimeException("Option number " + number + " ( " + message + " ) " + "out of range");
+        }
+    }
+
+    /**
+     * Waits until input event, if int seconds pass it passes
+     * @param seconds int seconds
+     * @return string option, or '\u0000' if time passes
+     */
+    public synchronized char waitEvent(int seconds) {
+        if (this.buffer.size() > 0) {
+            return this.buffer.poll();
+        } else {
+            try {
+                this.wait(seconds * 1000L);
+            } catch (InterruptedException ignored) {
+            }
+            return this.buffer.size() > 0 ? this.buffer.poll() : '\u0000';
+        }
+    }
+
+    /**
+     * Inserts char into Queue Character buffer
+     * @param c char c
+     */
+    synchronized void insertChar(char c) {
+        this.buffer.add(c);
+        this.notify();
     }
 
     /**
--- src/mytharena/gui/MythArenaGuiScreen.java
+++ src/mytharena/gui/MythArenaGuiScreen.java
@@ -2,6 +2,10 @@ package mytharena.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * MythArenaGuiScreen class extends JFrame
@@ -21,77 +25,45 @@ public class MythArenaGuiScreen extends JFrame {
      */
     private String descriptionText;
     /**
-     * String optionAButtonText
+     * String optionAText
      */
-    private String optionAButtonText;
+    private String optionAText;
     /**
-     * String optionBButtonText
+     * String optionBText
      */
-    private String optionBButtonText;
+    private String optionBText;
     /**
-     * String optionCButtonText
+     * String optionCText
      */
-    private String optionCButtonText;
+    private String optionCText;
     /**
-     * String optionDButtonText
+     * String optionDText
      */
-    private String optionDButtonText;
+    private String optionDText;
     /**
-     * String optionEButtonText
+     * String optionEText
      */
-    private String optionEButtonText;
+    private String optionEText;
     /**
-     * String optionFButtonText
+     * String optionFText
      */
-    private String optionFButtonText;
+    private String optionFText;
     /**
-     * String optionGButtonText
+     * String optionGText
      */
-    private String optionGButtonText;
+    private String optionGText;
     /**
-     * String optionHButtonText
+     * String optionHText
      */
-    private String optionHButtonText;
+    private String optionHText;
     /**
-     * String optionIButtonText
+     * String optionIText
      */
-    private String optionIButtonText;
+    private String optionIText;
     /**
-     * String optionJButtonText
+     * String optionJText
      */
-    private String optionJButtonText;
-    /**
-     * String optionAMessageText
-     */
-    private String optionAMessageText;
-    /**
-     * String optionBMessageText
-     */
-    private String optionBMessageText;
-    /**
-     * String optionALoginText
-     */
-    private String optionALoginText;
-    /**
-     * String optionBLoginText
-     */
-    private String optionBLoginText;
-    /**
-     * String optionARegisterText
-     */
-    private String optionARegisterText;
-    /**
-     * String optionBRegisterText
-     */
-    private String optionBRegisterText;
-    /**
-     * String optionAListText
-     */
-    private String optionAListText;
-    /**
-     * String optionBListText
-     */
-    private String optionBListText;
+    private String optionJText;
     /**
      * JPanel messagePanel
      */
@@ -236,6 +208,177 @@ public class MythArenaGuiScreen extends JFrame {
         this.optionBRegister = new JLabel();
         this.optionAList = new JLabel();
         this.optionBList = new JLabel();
+        this.addKeyListener(new KeyAdapter() {
+            /**
+             * keyPressed
+             * @param event KeyEvent event
+             */
+            public void keyPressed(KeyEvent event) {
+                MythArenaGuiScreen.this.formKeyPressed(event);
+            }
+        });
+        this.optionAButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionA();
+            }
+        });
+        this.optionBButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionB();
+            }
+        });
+        this.optionCButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionC();
+            }
+        });
+        this.optionDButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionD();
+            }
+        });
+        this.optionEButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionE();
+            }
+        });
+        this.optionFButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionF();
+            }
+        });
+        this.optionGButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionG();
+            }
+        });
+        this.optionHButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionH();
+            }
+        });
+        this.optionIButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionI();
+            }
+        });
+        this.optionJButton.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionJ();
+            }
+        });
+        this.optionAMessage.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionA();
+            }
+        });
+        this.optionBMessage.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionB();
+            }
+        });
+        this.optionALogin.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionA();
+            }
+        });
+        this.optionBLogin.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionB();
+            }
+        });
+        this.optionARegister.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionA();
+            }
+        });
+        this.optionBRegister.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionB();
+            }
+        });
+        this.optionAList.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionA();
+            }
+        });
+        this.optionBList.addMouseListener(new MouseAdapter() {
+            /**
+             * mouseClicked
+             * @param event MouseEvent event
+             */
+            public void mouseClicked(MouseEvent event) {
+                MythArenaGuiScreen.this.clickOptionB();
+            }
+        });
     }
 
     /**
@@ -273,6 +416,70 @@ public class MythArenaGuiScreen extends JFrame {
     }
 
     /**
+     * Sets specified option message
+     * @param number int number
+     * @param message String message
+     */
+    public void setOption(int number, String message) {
+        if (message == null) {
+            message = "";
+        } else {
+            message = this.reformatMessage(message);
+        }
+        switch (number) {
+            case 0 -> {
+                this.optionAText = message;
+                this.optionAButton.setVisible(!"".equals(message));
+                this.optionAMessage.setVisible(!"".equals(message));
+                this.optionALogin.setVisible(!"".equals(message));
+                this.optionARegister.setVisible(!"".equals(message));
+                this.optionAList.setVisible(!"".equals(message));
+            }
+            case 1 -> {
+                this.optionBText = message;
+                this.optionBButton.setVisible(!"".equals(message));
+                this.optionBMessage.setVisible(!"".equals(message));
+                this.optionBLogin.setVisible(!"".equals(message));
+                this.optionBRegister.setVisible(!"".equals(message));
+                this.optionBList.setVisible(!"".equals(message));
+            }
+            case 2 -> {
+                this.optionCText = message;
+                this.optionCButton.setVisible(!"".equals(message));
+            }
+            case 3 -> {
+                this.optionDText = message;
+                this.optionDButton.setVisible(!"".equals(message));
+            }
+            case 4 -> {
+                this.optionEText = message;
+                this.optionEButton.setVisible(!"".equals(message));
+            }
+            case 5 -> {
+                this.optionFText = message;
+                this.optionFButton.setVisible(!"".equals(message));
+            }
+            case 6 -> {
+                this.optionGText = message;
+                this.optionGButton.setVisible(!"".equals(message));
+            }
+            case 7 -> {
+                this.optionHText = message;
+                this.optionHButton.setVisible(!"".equals(message));
+            }
+            case 8 -> {
+                this.optionIText = message;
+                this.optionIButton.setVisible(!"".equals(message));
+            }
+            case 9 -> {
+                this.optionJText = message;
+                this.optionJButton.setVisible(!"".equals(message));
+            }
+        }
+        this.refreshLater();
+    }
+
+    /**
      * Reformats String message
      * @param message String message
      * @return String message
@@ -284,34 +491,6 @@ public class MythArenaGuiScreen extends JFrame {
         message = message.replaceAll("\n", "<br>");
         message = "<html>" + message + "</html>";
         return message;
-    }
-
-    /**
-     * Refresh components later
-     */
-    private void refreshLater() {
-        SwingUtilities.invokeLater(() -> {
-            MythArenaGuiScreen.this.titleLabel.setText(MythArenaGuiScreen.this.titleText);
-            MythArenaGuiScreen.this.descriptionLabel.setText(MythArenaGuiScreen.this.descriptionText);
-            MythArenaGuiScreen.this.optionAButton.setText(MythArenaGuiScreen.this.optionAButtonText);
-            MythArenaGuiScreen.this.optionBButton.setText(MythArenaGuiScreen.this.optionBButtonText);
-            MythArenaGuiScreen.this.optionCButton.setText(MythArenaGuiScreen.this.optionCButtonText);
-            MythArenaGuiScreen.this.optionDButton.setText(MythArenaGuiScreen.this.optionDButtonText);
-            MythArenaGuiScreen.this.optionEButton.setText(MythArenaGuiScreen.this.optionEButtonText);
-            MythArenaGuiScreen.this.optionFButton.setText(MythArenaGuiScreen.this.optionFButtonText);
-            MythArenaGuiScreen.this.optionGButton.setText(MythArenaGuiScreen.this.optionGButtonText);
-            MythArenaGuiScreen.this.optionHButton.setText(MythArenaGuiScreen.this.optionHButtonText);
-            MythArenaGuiScreen.this.optionIButton.setText(MythArenaGuiScreen.this.optionIButtonText);
-            MythArenaGuiScreen.this.optionJButton.setText(MythArenaGuiScreen.this.optionJButtonText);
-            MythArenaGuiScreen.this.optionAMessage.setText(MythArenaGuiScreen.this.optionAMessageText);
-            MythArenaGuiScreen.this.optionBMessage.setText(MythArenaGuiScreen.this.optionBMessageText);
-            MythArenaGuiScreen.this.optionALogin.setText(MythArenaGuiScreen.this.optionALoginText);
-            MythArenaGuiScreen.this.optionBLogin.setText(MythArenaGuiScreen.this.optionBLoginText);
-            MythArenaGuiScreen.this.optionARegister.setText(MythArenaGuiScreen.this.optionARegisterText);
-            MythArenaGuiScreen.this.optionBRegister.setText(MythArenaGuiScreen.this.optionBRegisterText);
-            MythArenaGuiScreen.this.optionAList.setText(MythArenaGuiScreen.this.optionAListText);
-            MythArenaGuiScreen.this.optionBList.setText(MythArenaGuiScreen.this.optionBListText);
-        });
     }
 
     /**
@@ -333,6 +512,132 @@ public class MythArenaGuiScreen extends JFrame {
             proportions[1] = destinationHeight;
         }
         return proportions;
+    }
+
+    /**
+     * Refresh components later
+     */
+    private void refreshLater() {
+        SwingUtilities.invokeLater(() -> {
+            MythArenaGuiScreen.this.titleLabel.setText(MythArenaGuiScreen.this.titleText);
+            MythArenaGuiScreen.this.descriptionLabel.setText(MythArenaGuiScreen.this.descriptionText);
+            MythArenaGuiScreen.this.optionAButton.setText(MythArenaGuiScreen.this.optionAText);
+            MythArenaGuiScreen.this.optionBButton.setText(MythArenaGuiScreen.this.optionBText);
+            MythArenaGuiScreen.this.optionCButton.setText(MythArenaGuiScreen.this.optionCText);
+            MythArenaGuiScreen.this.optionDButton.setText(MythArenaGuiScreen.this.optionDText);
+            MythArenaGuiScreen.this.optionEButton.setText(MythArenaGuiScreen.this.optionEText);
+            MythArenaGuiScreen.this.optionFButton.setText(MythArenaGuiScreen.this.optionFText);
+            MythArenaGuiScreen.this.optionGButton.setText(MythArenaGuiScreen.this.optionGText);
+            MythArenaGuiScreen.this.optionHButton.setText(MythArenaGuiScreen.this.optionHText);
+            MythArenaGuiScreen.this.optionIButton.setText(MythArenaGuiScreen.this.optionIText);
+            MythArenaGuiScreen.this.optionJButton.setText(MythArenaGuiScreen.this.optionJText);
+            MythArenaGuiScreen.this.optionAMessage.setText(MythArenaGuiScreen.this.optionAText);
+            MythArenaGuiScreen.this.optionBMessage.setText(MythArenaGuiScreen.this.optionBText);
+            MythArenaGuiScreen.this.optionALogin.setText(MythArenaGuiScreen.this.optionAText);
+            MythArenaGuiScreen.this.optionBLogin.setText(MythArenaGuiScreen.this.optionBText);
+            MythArenaGuiScreen.this.optionARegister.setText(MythArenaGuiScreen.this.optionAText);
+            MythArenaGuiScreen.this.optionBRegister.setText(MythArenaGuiScreen.this.optionBText);
+            MythArenaGuiScreen.this.optionAList.setText(MythArenaGuiScreen.this.optionAText);
+            MythArenaGuiScreen.this.optionBList.setText(MythArenaGuiScreen.this.optionBText);
+        });
+    }
+
+    /**
+     * Key pressed
+     * @param event KeyEvent event
+     */
+    private void formKeyPressed(KeyEvent event) {
+        if (event.getKeyCode() == 116) {
+            this.titleLabel.setText(this.titleText);
+            this.descriptionLabel.setText(this.descriptionText);
+            this.optionAButton.setText(this.optionAText);
+            this.optionBButton.setText(this.optionBText);
+            this.optionCButton.setText(this.optionCText);
+            this.optionDButton.setText(this.optionDText);
+            this.optionEButton.setText(this.optionEText);
+            this.optionFButton.setText(this.optionFText);
+            this.optionGButton.setText(this.optionGText);
+            this.optionHButton.setText(this.optionHText);
+            this.optionIButton.setText(this.optionIText);
+            this.optionJButton.setText(this.optionJText);
+            this.optionAMessage.setText(this.optionAText);
+            this.optionBMessage.setText(this.optionBText);
+            this.optionALogin.setText(this.optionAText);
+            this.optionBLogin.setText(this.optionBText);
+            this.optionARegister.setText(this.optionAText);
+            this.optionBRegister.setText(this.optionBText);
+            this.optionAList.setText(this.optionAText);
+            this.optionBList.setText(this.optionBText);
+        }
+    }
+
+    /**
+     * Clicks OptionA
+     */
+    private void clickOptionA() {
+        this.mythArenaGui.insertChar('A');
+    }
+
+    /**
+     * Clicks OptionB
+     */
+    private void clickOptionB() {
+        this.mythArenaGui.insertChar('B');
+    }
+
+    /**
+     * Clicks OptionC
+     */
+    private void clickOptionC() {
+        this.mythArenaGui.insertChar('C');
+    }
+
+    /**
+     * Clicks OptionD
+     */
+    private void clickOptionD() {
+        this.mythArenaGui.insertChar('D');
+    }
+
+    /**
+     * Clicks OptionE
+     */
+    private void clickOptionE() {
+        this.mythArenaGui.insertChar('E');
+    }
+
+    /**
+     * Clicks OptionF
+     */
+    private void clickOptionF() {
+        this.mythArenaGui.insertChar('F');
+    }
+
+    /**
+     * Clicks OptionG
+     */
+    private void clickOptionG() {
+        this.mythArenaGui.insertChar('G');
+    }
+    /**
+     * Clicks OptionH
+     */
+    private void clickOptionH() {
+        this.mythArenaGui.insertChar('H');
+    }
+
+    /**
+     * Clicks OptionI
+     */
+    private void clickOptionI() {
+        this.mythArenaGui.insertChar('I');
+    }
+
+    /**
+     * Clicks OptionJ
+     */
+    private void clickOptionJ() {
+        this.mythArenaGui.insertChar('J');
     }
 
     /**
