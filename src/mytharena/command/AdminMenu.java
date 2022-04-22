b97ff895b25cbeb77afc0d740f8e32b6c0c8364c
legendnightt <alex@legend.sh>
2022-04-22 21:20:04 +0200

    feat: admin menu display & log out

--- src/mytharena/command/AdminMenu.java
+++ src/mytharena/command/AdminMenu.java
@@ -24,9 +24,41 @@ public class AdminMenu extends Command {
      */
     @Override
     public void execute() {
-        super.getMythArenaGui().setButtonMode();
-        super.getMythArenaGui().setTitle("Welcome to Myth Arena " + super.getArena().getActiveUser().getUsername());
-        super.getMythArenaGui().setDescription("Admin mode");
+        while (super.getArena().getActiveUser() != null) {
+            super.getMythArenaGui().setButtonMode();
+            super.getMythArenaGui().setTitle("Welcome to Myth Arena " + super.getArena().getActiveUser().getUsername());
+            super.getMythArenaGui().setDescription("Admin Panel");
+            super.getMythArenaGui().setOption(0, "Manage admins");
+            super.getMythArenaGui().setOption(1, "Manage users");
+            super.getMythArenaGui().setOption(2, "Manage characters");
+            super.getMythArenaGui().setOption(3, "Validate combats");
+            super.getMythArenaGui().setOption(4, null);
+            super.getMythArenaGui().setOption(5, null);
+            super.getMythArenaGui().setOption(6, null);
+            super.getMythArenaGui().setOption(7, null);
+            super.getMythArenaGui().setOption(8, "Log out");
+            super.getMythArenaGui().setOption(9, null);
+            switch (super.getMythArenaGui().waitEvent(30)) {
+                // manage admins
+                case 'A' -> {
+
+                }
+                // manage users
+                case 'B' -> {
+
+                }
+                // manage characters
+                case 'C' -> {
+
+                }
+                // validate combats
+                case 'D' -> {
+
+                }
+                // log out
+                case 'I' -> super.getArena().setActiveUser(null);
+            }
+        }
     }
 
 }
