8aa463ad58bb7077d44c6740917d737af9be3972
legendnightt <alex@legend.sh>
2022-04-23 01:44:52 +0200

    feat: admin menu manage admins

--- src/mytharena/command/AdminMenu.java
+++ src/mytharena/command/AdminMenu.java
@@ -2,7 +2,12 @@ package mytharena.command;
 
 import mytharena.Arena;
 import mytharena.data.Data;
+import mytharena.data.user.Admin;
+import mytharena.data.user.User;
 import mytharena.gui.MythArenaGui;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * AdminMenu class extends Command
@@ -40,9 +45,7 @@ public class AdminMenu extends Command {
             super.getMythArenaGui().setOption(9, null);
             switch (super.getMythArenaGui().waitEvent(30)) {
                 // manage admins
-                case 'A' -> {
-
-                }
+                case 'A' -> this.manageAdmins();
                 // manage users
                 case 'B' -> {
 
@@ -57,6 +60,98 @@ public class AdminMenu extends Command {
                 }
                 // log out
                 case 'I' -> super.getArena().setActiveUser(null);
+            }
+        }
+    }
+
+    /**
+     * Manage admins
+     */
+    private void manageAdmins() {
+        boolean exit = false;
+        while (!exit) {
+            this.getMythArenaGui().setListMode();
+            super.getMythArenaGui().setTitle("Admin Manager Tool");
+            super.getMythArenaGui().setDescription("Select what you want to change");
+            super.getMythArenaGui().setOption(0, "Unregister selected admin");
+            super.getMythArenaGui().setOption(1, "Register new admin");
+            super.getMythArenaGui().setOption(2, "Back to AdminMenu");
+            super.getMythArenaGui().setOption(3, null);
+            ArrayList<User> adminArrayList = new ArrayList<>();
+            ArrayList<String> adminUsernameArrayList = new ArrayList<>();
+            for (User user : super.getData().getUserArrayList()) {
+                if (user instanceof Admin) {
+                    adminArrayList.add(user);
+                    adminUsernameArrayList.add(user.getUsername());
+                }
+            }
+            super.getMythArenaGui().setList(adminUsernameArrayList);
+            switch (super.getMythArenaGui().waitEvent(30)) {
+                // remove selected admin
+                case 'A' -> {
+                    int selected = super.getMythArenaGui().getLastSelectedListIndex();
+                    super.getMythArenaGui().setDescription("Removed selected admin: " + selected);
+                    super.getData().getUserArrayList().remove(adminArrayList.get(selected));
+                    super.getMythArenaGui().waitEvent(1);
+                }
+                // register admin
+                case 'B' -> this.registerAdmin();
+                case 'C' -> exit = true;
+            }
+        }
+    }
+
+    /**
+     * Register a admin into UserArrayList
+     */
+    private void registerAdmin() {
+        super.getMythArenaGui().setFormMode();
+        super.getMythArenaGui().setTitle("Admin Register Form");
+        super.getMythArenaGui().setDescription("Fill the fields for creating an admin account");
+        super.getMythArenaGui().setOption(0, "Back to Admin Manager Tool");
+        super.getMythArenaGui().setOption(0, "Register Admin");
+        super.getMythArenaGui().setField(0, "Username");
+        super.getMythArenaGui().setField(1, "Password");
+        super.getMythArenaGui().setField(2, null);
+        boolean exitRegisterAdmin = false;
+        while (!exitRegisterAdmin) {
+            switch (super.getMythArenaGui().waitEvent(30)) {
+                // exits register admin
+                case 'A' -> exitRegisterAdmin = true;
+                // tries to create admin user
+                case 'B' -> {
+                    String user = super.getMythArenaGui().getFieldText(0);
+                    String pass = super.getMythArenaGui().getFieldText(1);
+                    // checks if all fields have at least 4 characters
+                    if (user == null || user.length() < 4 && pass == null || pass.length() < 4) {
+                        super.getMythArenaGui().setDescription("All fields must be filled in to register. Fields must have at least 4 characters");
+                    } else {
+                        // checks if username is taken
+                        boolean isUnique = true;
+                        int i = 0;
+                        while (i < super.getData().getUserArrayList().size() && isUnique) {
+                            if (user.equals(super.getData().getUserArrayList().get(i).getUsername())){
+                                super.getMythArenaGui().setDescription("Username is taken");
+                                isUnique = false;
+                            }
+                            i++;
+                        }
+                        // creates admin user and save it in data serializing it
+                        if (isUnique) {
+                            try {
+                                super.getData().getUserArrayList().add(new Admin(user, pass, super.getData()));
+                                super.getArena().serializeData();
+                                super.getMythArenaGui().setDescription("Admin account has been created successfully!");
+                                super.getMythArenaGui().waitEvent(1);
+                                super.getMythArenaGui().clearFieldText(0);
+                                super.getMythArenaGui().clearFieldText(1);
+                                exitRegisterAdmin = true;
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
             }
         }
     }
