307ca101de7afc0cfdbdd01d57f847d48a973698
gutche <gledrianregalagutierrez@gmail.com>
2022-04-25 16:48:03 +0200

    fix: selectEquipment now fully functioning

--- src/mytharena/command/PlayerMenu.java
+++ src/mytharena/command/PlayerMenu.java
@@ -3,6 +3,7 @@ package mytharena.command;
 import mytharena.Arena;
 import mytharena.data.Data;
 import mytharena.data.character.inventory.equipment.Equipment;
+import mytharena.data.character.inventory.equipment.Weapon;
 import mytharena.data.combat.PendingCombat;
 import mytharena.data.notification.GeneralNotification;
 import mytharena.data.notification.Notification;
@@ -75,8 +76,9 @@ public class PlayerMenu extends Command {
             boolean exit = false;
             while (!exit) {
                 getMythArenaGui().setListMode();
+                getMythArenaGui().setTitle("Gold history");
                 ArrayList<String> message = new ArrayList<>();
-                message.add(Integer.toString(player.getCharacter().getGold()) + " gold");
+                message.add("Current gold: " + Integer.toString(player.getCharacter().getGold()) + " gold");
                 message.add("Gold lost in battle: 2M");
                 message.add("Gold won in battle: 1M");
                 getMythArenaGui().setList(message);
@@ -98,6 +100,7 @@ public class PlayerMenu extends Command {
         boolean exit = false;
         while (!exit) {
             getMythArenaGui().setListMode();
+            getMythArenaGui().setTitle("Notifications");
             getMythArenaGui().setOption(0, null);
             getMythArenaGui().setOption(1, null);
             getMythArenaGui().setOption(2, "Back");
@@ -236,65 +239,119 @@ public class PlayerMenu extends Command {
     public void selectEquipment() {
         if (player.getCharacter() != null) {
             getMythArenaGui().setListMode();
-            getMythArenaGui().setOption(0, null);
-            boolean isFinished = false;
-            while (!isFinished) {
+            boolean exit = false;
+            while (!exit) {
+                getMythArenaGui().setOption(0, "Unequip all");
                 getMythArenaGui().setOption(1, "Equip");
                 getMythArenaGui().setOption(2, "Cancel");
                 getMythArenaGui().setOption(3, "Next");
                 ArrayList<String> listWeapons = new ArrayList<>();
                 getMythArenaGui().setDescription("Select your weapons. TIP: You may equip two single-handed weapons simultaneously");
-                for (Equipment weapon : player.getCharacter().getInventory().getWeaponArrayList()) {
-                        listWeapons.add(weapon.getName());
+                for (Equipment item : player.getCharacter().getInventory().getWeaponArrayList()) {
+                        Weapon weapon = (Weapon) item;
+                        listWeapons.add(weapon.getName() + "     Type: " + (weapon.isTwoHands() ? "Two-hander" : "One-hander") + "     " + "Offense: " + Integer.toString(weapon.getAttackModification()) + "     " + "Defense: " + Integer.toString(weapon.getDefenseModification()));
                 }
                 listWeapons.add("----------------------------------------------------------------------------------");
-                listWeapons.add("Weapon slots:");
+                listWeapons.add("Current weapons:");
+
                 int equippedWeaponCount = 0;
                 for (Equipment equippedWeapon: player.getCharacter().getEquippedWeaponArrayList()) {
                     listWeapons.add("Slot "+ Integer.toString(equippedWeaponCount+1) +": "+ equippedWeapon.getName());
                     equippedWeaponCount++;
                 }
-                if (equippedWeaponCount < 2) {
-                    listWeapons.add("Slot "+ Integer.toString(equippedWeaponCount+1) +": Free");
+
+                int freeSlotCount = player.getCharacter().getEquippedWeaponArrayList().size();
+                while (freeSlotCount < 2) {
+                    listWeapons.add("Slot "+ Integer.toString(freeSlotCount+1) +": Free");
+                    freeSlotCount++;
                 }
 
                 getMythArenaGui().setList(listWeapons);
                 char choice = getMythArenaGui().waitEvent(30);
                 if (choice == 'D') {
-                    getMythArenaGui().setDescription("Select your armor");
-                    getMythArenaGui().setOption(1,null);
-                    getMythArenaGui().setOption(3,"Finish");
-                    getMythArenaGui().setOption(2,"Back");
-                    ArrayList<String> listArmor = new ArrayList<>();
-                    for (Equipment armor : player.getCharacter().getInventory().getArmorArrayList()) {
-                        listArmor.add(armor.getName());
-                    }
-                    getMythArenaGui().setList(listArmor);
-                    if (getMythArenaGui().waitEvent(30) == 'D') {
-                        isFinished = true;
+                    if(!player.getCharacter().getEquippedWeaponArrayList().isEmpty()) {
+                        getMythArenaGui().setDescription("Select your armor");
+                        getMythArenaGui().setOption(0,null);
+                        getMythArenaGui().setOption(1, "Equip");
+                        getMythArenaGui().setOption(2, "Back");
+                        getMythArenaGui().setOption(3, "Finish");
+
+                        boolean isFinished = false;
+                        while(!isFinished) {
+                            ArrayList<String> listArmor = new ArrayList<>();
+                            for (Equipment armor : player.getCharacter().getInventory().getArmorArrayList()) {
+                                listArmor.add(armor.getName());
+                            }
+                            listArmor.add("----------------------------------------------------------------------------------");
+                            listArmor.add("Current armor:");
+                            listArmor.add(player.getCharacter().getArmor().getName());
+                            getMythArenaGui().setList(listArmor);
+                            char option = getMythArenaGui().waitEvent(30);
+                            if (option == 'D') {
+                                try {
+                                    getArena().serializeData();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                isFinished = true;
+                                exit = true;
+                            } else if (option == 'B') {
+                                int index = getMythArenaGui().getLastSelectedListIndex();
+                                if (index != -1) {
+                                    player.getCharacter().setArmor(player.getCharacter().getInventory().getArmorArrayList().get(index));
+                                }
+                            } else if (option == 'C') {
+                                isFinished = true;
+                            }
+                        }
+                    }else {
+                        getMythArenaGui().setDescription("You must equip at least one weapon to continue");
+                        getMythArenaGui().waitEvent(2);
                     }
                 } else if (choice == 'C') {
-                    isFinished = true;
+                    exit = true;
                 }else if (choice == 'B') {
-                    int listIndex = getMythArenaGui().getLastSelectedListIndex();
-                    if (listIndex < 3 && listIndex != -1) {
-                        Equipment weapon = player.getCharacter().getInventory().getWeaponArrayList().get(listIndex);
-                        if (!player.getCharacter().getEquippedWeaponArrayList().contains(weapon)) {
-                            int size = player.getCharacter().getEquippedWeaponArrayList().size();
-                            if (size < 2) {
-                                player.getCharacter().getEquippedWeaponArrayList().add(1, player.getCharacter().getInventory().getWeaponArrayList().get(listIndex));
-                            } else {
-                                player.getCharacter().getEquippedWeaponArrayList().set(1, player.getCharacter().getEquippedWeaponArrayList().get(0));
-                                player.getCharacter().getEquippedWeaponArrayList().set(0, player.getCharacter().getInventory().getWeaponArrayList().get(listIndex));
+                    if (!player.getCharacter().getEquippedWeaponArrayList().isEmpty()) {
+                        // If character has equipped weapon
+                        Weapon currentWeapon = (Weapon) player.getCharacter().getEquippedWeaponArrayList().get(0);
+                        if (!currentWeapon.isTwoHands()) {
+                            int listIndex = getMythArenaGui().getLastSelectedListIndex();
+                            if (listIndex < 3 && listIndex != -1) {
+                                Equipment item = player.getCharacter().getInventory().getWeaponArrayList().get(listIndex);
+                                Weapon weapon = (Weapon) item;
+                                if (!player.getCharacter().getEquippedWeaponArrayList().contains(weapon)) {
+                                    if (!weapon.isTwoHands()) {
+                                        int size = player.getCharacter().getEquippedWeaponArrayList().size();
+                                        if (size < 2) {
+                                            player.getCharacter().getEquippedWeaponArrayList().add(1, player.getCharacter().getInventory().getWeaponArrayList().get(listIndex));
+                                        } else {
+                                            player.getCharacter().getEquippedWeaponArrayList().set(1, player.getCharacter().getEquippedWeaponArrayList().get(0));
+                                            player.getCharacter().getEquippedWeaponArrayList().set(0, player.getCharacter().getInventory().getWeaponArrayList().get(listIndex));
+                                        }
+                                    }else {
+                                        getMythArenaGui().setDescription("You must unequip your weapon to use a two-handed weapon!");
+                                        getMythArenaGui().waitEvent(2);
+                                    }
+                                }
+                            }
+                        } else {
+                            getMythArenaGui().setDescription("You currently have a two-handed weapon equipped. You can only use two single-handers for dual-wielding");
+                            getMythArenaGui().waitEvent(5);
+                        }
+                    }else {
+                        // If no equipped weapons
+                        int listIndex = getMythArenaGui().getLastSelectedListIndex();
+                        if (listIndex < 3 && listIndex != -1) {
+                            Equipment item = player.getCharacter().getInventory().getWeaponArrayList().get(listIndex);
+                            Weapon weapon = (Weapon) item;
+                            if (!player.getCharacter().getEquippedWeaponArrayList().contains(weapon)) {
+                                player.getCharacter().getEquippedWeaponArrayList().add(weapon);
                             }
                         }
                     }
+                }else if(choice == 'A') {
+                    player.getCharacter().getEquippedWeaponArrayList().clear();
                 }
-            }
-            try {
-                getArena().serializeData();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }else {
             getMythArenaGui().setDescription("No character found");
